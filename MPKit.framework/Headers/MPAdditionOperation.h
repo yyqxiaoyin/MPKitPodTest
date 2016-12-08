90e20cc1347472ecbacbffc23ca16aae78cf86fb
Mopon <357491060@qq.com>
2016-12-08 10:54:09 +0800

    init

---
+++ MPKit.framework/Headers/MPAdditionOperation.h
@@ -0,0 +1,15 @@
+//
+//  MPAdditionOperation.h
+//  MPKit
+//
+//  Created by Mopon on 2016/12/8.
+//  Copyright © 2016年 Mopon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPAdditionOperation : NSObject
+
++(NSInteger)additionNum1:(NSInteger)num1 num2:(NSInteger)num2;
+
+@end
---
+++ MPKit.framework/Headers/MPKit.h
@@ -0,0 +1,18 @@
+//
+//  MPKit.h
+//  MPKit
+//
+//  Created by Mopon on 2016/12/8.
+//  Copyright © 2016年 Mopon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPKit.
+FOUNDATION_EXPORT double MPKitVersionNumber;
+
+//! Project version string for MPKit.
+FOUNDATION_EXPORT const unsigned char MPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPKit/PublicHeader.h>
+#import <MPKit/MPAdditionOperation.h>
